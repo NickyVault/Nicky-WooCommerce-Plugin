40ca734cec95d75d77c211d097dfeb054f61c68e
itmilos <mikeruje@gmail.com>
2026-04-06 14:16:18 -0400

    Update 1.11

--- Nicky/nicky-me.php
+++ Nicky/nicky-me.php
@@ -3,7 +3,7 @@
  * Plugin Name: Nicky.me
  * Plugin URI: https://github.com/NickyVault/Nicky-WooCommerce-Plugin
  * Description: Secure and reliable payment processing for WooCommerce powered by Nicky.me.
- * Version: 1.10
+ * Version: 1.11
  * Author: Nicky.me
  * Author URI: https://nicky.me
  * License: GPL v2 or later
@@ -24,7 +24,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define plugin constants
-define('NICKY_PAYMENT_GATEWAY_VERSION', '1.10');
+define('NICKY_PAYMENT_GATEWAY_VERSION', '1.11');
 define('NICKY_PAYMENT_GATEWAY_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('NICKY_PAYMENT_GATEWAY_PLUGIN_PATH', plugin_dir_path(__FILE__));
 
